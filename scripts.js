5773271dfb018482323490224d4836b83326d77a
Chocol8 <zjbarcellano@gmail.com>
2024-05-05 19:14:32 +0800

    Added function to display books in array

--- scripts.js
+++ scripts.js
@@ -1,16 +1,30 @@
-const myLibrary = [];
+const myLibrary = ["ehe","tehe"];
 const insertBtn = document.querySelector('.insert');
+const book = document.querySelector('#book');
+const content = document.querySelector('.book-wrapper')
 
 function addBookToLibrary(buko) {
     myLibrary.push(buko);
 }
 
 insertBtn.addEventListener('click',function(){
-    const book = document.querySelector('#book');
     if(book.value){
         addBookToLibrary(book.value);
     }
     else{
         alert("Empty input");
     }
+});
+
+myLibrary.forEach(book => {
+    let card = document.createElement("div");
+    let header = document.createElement("h1");
+    let para = document.createElement("p");
+    let remove = document.createElement("button");
+    remove.classList.add("delet");
+    card.classList.add("card");
+    content.appendChild(card);
+    card.appendChild(header);
+    card.appendChild(para);
+    para.textContent = book;
 });
