97f94f0af600421a07be1d3916c63cdd7e302c48
Chocol8 <zjbarcellano@gmail.com>
2024-05-07 18:50:12 +0800

    Removed initial add book function and added new object for books. Edited display function to display using new book object.

--- scripts.js
+++ scripts.js
@@ -1,42 +1,63 @@
-const myLibrary = ["ehe","tehe"];
-const insertBtn = document.querySelector('.insert');
-const book = document.querySelector('#book');
+const myLibrary = [];
 const content = document.querySelector('.book-wrapper');
 const nu = document.querySelector(".new");
 const foam = document.querySelector(".foam");
-const close = document.querySelector(".close");
+const closed = document.querySelector(".close");
+const sub = document.querySelector('.submit');
+let bookNum = 1;
+
+function book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+let sample1 = new book('sample1','auth',2024,'yes');
+let sample2 = new book('sample2','auth1',2023,'yes');
 
 function addBookToLibrary(buko) {
     myLibrary.push(buko);
 }
 
-insertBtn.addEventListener('click',function(){
-    if(book.value){
-        addBookToLibrary(book.value);
-    }
-    else{
-        alert("Empty input");
-    }
-});
+addBookToLibrary(sample1);
+addBookToLibrary(sample2);
 
 nu.onclick = function(){
     foam.showModal();
     foam.disabled = true;
 }
 
-close.onclick = function(){
+closed.onclick = function(){
     foam.close();
 }
 
+sub.addEventListener("click",function(){
+    const title = document.querySelector('#title');
+    
+
+    myLibrary.push();
+});
+
 myLibrary.forEach(book => {
     let card = document.createElement("div");
-    let header = document.createElement("h1");
-    let para = document.createElement("p");
+    let titleHeader = document.createElement("h1");
+    let author = document.createElement("p");
+    let pages = document.createElement("p");
+    let isRead = document.createElement("p");
     let remove = document.createElement("button");
     remove.classList.add("delet");
     card.classList.add("card");
+    titleHeader.classList.add("title-header");
     content.appendChild(card);
-    card.appendChild(header);
-    card.appendChild(para);
-    para.textContent = book;
+    card.appendChild(titleHeader);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(isRead);
+    card.appendChild(remove);
+    remove.textContent = `remove`;
+    titleHeader.textContent = book.title;
+        author.textContent =  `Author: ` + book.author;
+        pages.textContent = `Number of pages: ` + book.pages;
+        isRead.textContent = `Is read: ` + book.read;
 });
